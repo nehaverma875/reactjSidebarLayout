a86a09e4c016b31b9af91841124fd7d7c699d507
Neha verma <72869767+nehaverma875@users.noreply.github.com>
2024-04-15 15:19:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route,NavLink } from 'react-router-dom';
+import { Home } from './component/Home';
+import { Leads } from './component/Leads';
+import { Sales } from './component/Sales';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className='flex'>
+      {/* Sidebar */}
+      <ul className='bg-blue-500 h-screen w-20'>
+        <li>
+          <NavLink to="/" activeClassName="active">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/leads" activeClassName="active">Leads</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sales" activeClassName="active">Sales</NavLink>
+        </li>
+      </ul>
+
+      {/* Main Content */}
+      <div className='flex-grow'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/leads" element={<Leads />} />
+          <Route path="/sales" element={<Sales />} />
+        </Routes>
+      </div>
     </div>
+  </Router>
   );
 }
 
